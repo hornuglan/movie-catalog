2cf76431a41e1c5927d5af5fd91270121a60ce47
hornuglan <rita@nextmail.ru>
2020-04-24 18:37:22 +0300

    Feature: changing app theme by click on a button

--- app/src/main/java/com/example/moviecatalogue/MainActivity.kt
+++ app/src/main/java/com/example/moviecatalogue/MainActivity.kt
@@ -1,8 +1,11 @@
 package com.example.moviecatalogue
 
+import android.content.Context
 import android.content.Intent
+import android.content.SharedPreferences
 import android.net.Uri
 import android.os.Bundle
+import android.view.View
 import android.widget.Button
 import android.widget.ListView
 import android.widget.TextView
@@ -15,10 +18,24 @@ class MainActivity : AppCompatActivity() {
 
     private lateinit var listView: ListView
     private lateinit var inviteFriendButton: Button
+    private lateinit var changeThemeButton: Button
 
+    private lateinit var sharedPreferences: SharedPreferences
+    private val themeKey = "currentTheme"
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        sharedPreferences = getSharedPreferences(
+            "ThemePref",
+            Context.MODE_PRIVATE
+        )
+
+        when (sharedPreferences.getString(themeKey, "appTheme")) {
+            "light" -> theme.applyStyle(R.style.Light, true)
+            "dark" ->  theme.applyStyle(R.style.Dark, true)
+        }
+
         setContentView(R.layout.activity_main)
 
         val movies = ArrayList<Movie>()
@@ -82,5 +99,18 @@ class MainActivity : AppCompatActivity() {
         val intent = Intent(Intent.ACTION_SENDTO, Uri.parse("mailto:"))
         intent.putExtra(Intent.EXTRA_TEXT, "Hello! Join me in this Movies Catalogue App:-)")
         this.startActivity(intent)
+    }
+
+    fun changeTheme(view: View) {
+
+        when(sharedPreferences.getString(themeKey, "appTheme")) {
+            "dark" -> sharedPreferences.edit().putString(themeKey, "light").apply()
+            "light" -> sharedPreferences.edit().putString(themeKey, "dark").apply()
+        }
+
+        val intent = intent
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP)
+        finish()
+        startActivity(intent)
     }
 }
