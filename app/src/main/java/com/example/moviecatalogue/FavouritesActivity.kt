5c6d28d17c1e0e2da9cfd91ad118ae6d8921e297
hornuglan <rita@nextmail.ru>
2020-05-04 09:38:35 +0300

    Feature: clicklisteners to see movie details and delete item from favourites list

--- app/src/main/java/com/example/moviecatalogue/FavouritesActivity.kt
+++ app/src/main/java/com/example/moviecatalogue/FavouritesActivity.kt
@@ -1,5 +1,6 @@
 package com.example.moviecatalogue
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.LayoutInflater
@@ -29,9 +30,31 @@ class FavouritesActivity : AppCompatActivity() {
         val recyclerView = findViewById<RecyclerView>(R.id.list_favourites)
         val layoutManager = LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false)
         recyclerView.layoutManager = layoutManager
-        recyclerView.adapter = FavouritesAdapter(LayoutInflater.from(this), favourites)
+        recyclerView.adapter = FavouritesAdapter(LayoutInflater.from(this), favourites, object : FavouritesAdapter.OnMovieClickListener {
+            override fun onDetailsButtonClickListener(movieItem: MovieItem) {
+                openPreview(movieItem.title, movieItem.poster)
+            }
+
+            override fun onFavouritesButtonClickListener(
+                movieItem: MovieItem,
+                position: Int,
+                removeFromFavouritesView: ImageView
+            ) {
+                favourites.removeAt(position)
+                recyclerView.adapter?.notifyItemRemoved(position)
+            }
+        })
 
         val itemDecoration = DividerItemDecoration(this, DividerItemDecoration.VERTICAL)
         recyclerView.addItemDecoration(itemDecoration)
+    }
+
+    fun openPreview(movieTitle: Int, moviePoster: Int) {
+        val intent = Intent(this, MovieDetailsActivity::class.java)
+        val b = Bundle()
+        b.putInt("movieTitle", movieTitle)
+        b.putInt("moviePoster", moviePoster)
+        intent.putExtras(b)
+        this.startActivity(intent)
     }
 }
--- app/src/main/java/com/example/moviecatalogue/FavouritesAdapter.kt
+++ app/src/main/java/com/example/moviecatalogue/FavouritesAdapter.kt
@@ -6,16 +6,27 @@ import android.view.ViewGroup
 import android.widget.ImageView
 import androidx.recyclerview.widget.RecyclerView
 
-class FavouritesAdapter(val inflater: LayoutInflater, val items: ArrayList<MovieItem>) : RecyclerView.Adapter<com.example.moviecatalogue.MovieItemViewHolder>() {
+class FavouritesAdapter(val inflater: LayoutInflater, val items: ArrayList<MovieItem>, val listener: OnMovieClickListener) : RecyclerView.Adapter<com.example.moviecatalogue.MovieItemViewHolder>() {
 
     override fun getItemCount() = items.size
 
-    override fun onBindViewHolder(holder: com.example.moviecatalogue.MovieItemViewHolder, position: Int) {
+    override fun onBindViewHolder(holder: MovieItemViewHolder, position: Int) {
         val item = items[position]
         holder.bind(item)
+
+        val detailsButton = holder.itemView.findViewById<View>(R.id.movie_details_button)
+        detailsButton.setOnClickListener { listener.onDetailsButtonClickListener(item) }
+
+        val removeFromFavouritesView = holder.itemView.findViewById<ImageView>(R.id.add_to_favourites_button)
+        removeFromFavouritesView.setOnClickListener { listener.onFavouritesButtonClickListener(item, position, removeFromFavouritesView) }
     }
 
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): com.example.moviecatalogue.MovieItemViewHolder {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MovieItemViewHolder {
         return MovieItemViewHolder(inflater.inflate(R.layout.item_movie, parent, false))
+    }
+
+    interface OnMovieClickListener {
+        fun onDetailsButtonClickListener(movieItem: MovieItem)
+        fun onFavouritesButtonClickListener(movieItem: MovieItem, position: Int, removeFromFavouritesView: ImageView)
     }
 }
